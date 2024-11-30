71f4aa230008719cc566aeb3c17906322ac09af3
marto-92 <mar7in.kamenov@gmail.com>
2024-11-30 10:26:00 +0200

    day 33 tasks added

---
+++ day-33/task_2.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+
+
+MY_LAT = 43.1915198
+MY_LONG = 23.6541707
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sunrise)
+print(sunset)
+print(time_now.hour)
